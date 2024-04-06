f6c517c0f4a4d9102bb2cb702ac0b9b7a7a4e6fe
Sarawoot Pattamapong <ponlovet@hotmail.com>
2024-04-07 01:56:50 +0700

    bug fix

--- PaySolutions/Base/Controller/Jumpapp/Request.php
+++ PaySolutions/Base/Controller/Jumpapp/Request.php
@@ -67,15 +67,12 @@ class Request extends \Magento\Framework\App\Action\Action
 
         // Mode
         echo "Pay Solution Request<br>";
-        $cardtype = $this->request->getParam('cardtype');
-        echo $cardtype;
-
-        //$shopeeUrl = 'https://api.uat.wallet.airpay.co.th/v3/merchant-host/order/create'; //sandbox url
-
+        $paysoUrl = 'https://payment.paysolutions.asia/epaylink/payment.aspx';
         $merchantId = $this->scopeConfig->getValue('payment/payso_payment/merchant_id', $storeScope);
+        $returnUrl = $baseWebsiteUrl."paysopayment/callback/postback/";
+        $postbackUrl = $baseWebsiteUrl."paysopayment/callback/returnurl/";
 
-        $returnUrl = $baseWebsiteUrl."paysopayment/callbacl/postback/";;
-        $postbackUrl = $baseWebsiteUrl."paysopayment/callbacl/returnurl/";;
+        echo $paysoUrl.'<br><br>';
 
         //Order Details
         $order = $this->_checkoutSession->getLastRealOrder();
@@ -90,70 +87,103 @@ class Request extends \Magento\Framework\App\Action\Action
         $grandTotal = $order->getGrandTotal(); //1234.5600
         $amount = number_format((float)$grandTotal, 2, '.', ''); //1234.56
         $product = $order->getAllItems();
+        $orderProduct = ''; $productFlag = 0;
         foreach ($product as $item) {
             $itemNo = $item->getId();
             $productId = $item->getProductId();
             $productName = $this->getProduct($productId)->getName();
-            echo "Item".$itemNo." ".$productName;       
-            echo "<br>";
-         }
+            if($productFlag > 0){
+                $orderProduct = $orderProduct.', ';
+            }
+            $orderProduct = $orderProduct.$productName;
+            $productFlag = 1;
+            
+        }
+        echo "<br>";
 
-        //Signature Hash(sha256)
+        //----Get Payment and change to Payso payment channel code----
+        $paymentMethod = $order->getPayment()->getMethod(); 
+        $channel = '';
+        if($paymentMethod == "payso_card"){ 
+            $cardGetParam = $this->request->getParam('cardtype'); // Get selected card type from url
+            if($cardGetParam == null){
+                $channel = 'full';
+            }else{
+                $channel = $cardGetParam;
+            }
+        }elseif ($paymentMethod == "payso_installment") {
+            $channel = "installment";
+        }elseif ($paymentMethod == "payso_bill") {
+            $channel = "bill";
+        }elseif ($paymentMethod == "payso_promptpay") {
+            $channel = "promptpay";
+        }elseif ($paymentMethod == "payso_alipay") {
+            $channel = "alipay";
+        }elseif ($paymentMethod == "payso_wechat") {
+            $channel = "wechat";
+        }elseif ($paymentMethod == "payso_truewallet") {
+            $channel = "truewallet";
+        }elseif ($paymentMethod == "payso_ibanking") {
+            $bankGetParam = $this->request->getParam('ibank'); // Get selected bank type from url
+            if($bankGetParam == null){
+                $channel = "ibanking";
+            }else{
+                $channel = $bankGetParam;
+            }
+        }else{
+            $channel = "";
+        }
+        echo $channel."<br>";
+
+        // Request data
         $bodyEx = '{
                     "merchantid":"'.$merchantId.'",
                     "refno":"'.$refno.'",
                     "customeremail":"'.$customerEmail.'",
-                    "productdetail":"null",
-                    "total":'.$amount.',
+                    "productdetail":"'.$orderProduct.'",
+                    "total":"'.$amount.'",
                     "lang":"TH",
                     "cc":"00",
-                    "channel":"",
+                    "channel":"'.$channel.'",
                     "returnurl":"'.$returnUrl.'",
                     "postbackurl":"'.$postbackUrl.'"
                 }';
         print_r($bodyEx);
-        die();
+        echo "<br><br><br>";
 
-        $hash = hash_hmac('sha256', $bodyEx, $secretKey, true);
-        $signature = base64_encode($hash);
-
-        //Send Api Request
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => $shopeeUrl ,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS =>$bodyEx,
-        CURLOPT_HTTPHEADER => array(
-            'X-Airpay-ClientId: '.$clientID,
-            'X-Airpay-Req-H: '.$signature,
-            'Content-Type: text/plain'
-        ),
-        ));
-
-        $response = curl_exec($curl);
-        curl_close($curl);
-        
-        $data = json_decode($response, true);
-        $errorCode = $data['errcode'];
-        $debugMsg = $data['debug_msg'];
-        $redirectUrl = $data['redirect_url_http'];
-
-
-        if( $errorCode != 0){
-            $this->messageManager->addError(__("Error(".$errorCode.")".$debugMsg."): Cannot connect Shopee Pay service. ไม่สามารถเชื่อมต่อ Shopee Pay กรุณาลองใหม่อีกครั้ง"));
-            $redirect->setUrl($baseWebsiteUrl);
-            return $redirect;
-        }
+        //---- Create Form data and redirect
+        echo '<form method="post" action="https://payment.paysolutions.asia/epaylink/payment.aspx"><br>';
+            echo 'Customer E-mail:';
+            echo '<input type="text" name="customeremail" value="'.$customerEmail.'">';
+            echo '<br> Product Detail:';
+            echo '<input type="text" name="productdetail" value="'.$orderProduct.'">';
+            echo '<br>';
+            echo 'Reference No.:';
+            echo '<input type="text" name="refno" value="'.$refno.'">';
+            echo '<br>';
+            echo 'Merchant ID:';
+            echo '<input type="text" name="merchantid" value="'.$merchantId.'">';
+            echo '<br>';
+            echo 'Currency Code:';
+            echo '<input type="text" name="cc" value="00">';
+            echo '<br> Total:';
+            echo '<input type="text" name="total" value="'.$amount.'">';
+            echo '<br> Lang:';
+            echo '<input type="text" name="lang" value="TH">';
+            echo '<br> Channel:';
+            echo '<input type="text" name="channel" value="'.$channel.'">';
+            echo '<br> Return Url:';
+            echo '<input type="text" name="returnurl" value="'.$returnUrl.'">';
+            echo '<br> Postback Url:';
+            echo '<input type="text" name="postbackurl" value="'.$postbackUrl.'">';
+            echo '<br>';
+            echo '<br>';
+            echo '<br>';
+            echo '<input type="submit" name="Submit" value="Comfirm Order">';
+        echo '</form>';
 
 
-        $redirect->setUrl($redirectUrl);
-        return $redirect;
+        return;
     }
 
     public function getProduct($id)

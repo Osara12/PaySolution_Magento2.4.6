4314e2b27018a1735afb3955fc373d099a3c18a7
Sarawoot Pattamapong <ponlovet@hotmail.com>
2024-04-11 02:59:14 +0700

    fix

--- PaySolutions/Base/Model/Api/PostbackRepository.php
+++ PaySolutions/Base/Model/Api/PostbackRepository.php
@@ -22,6 +22,29 @@ class PostbackRepository implements PostbackInterface
         $this->request = $request;
 
     }
+
+    public function lineNotify($msg){
+        $url = "https://notify-api.line.me/api/notify";
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $headers = array(
+            "Content-Type: application/x-www-form-urlencoded",
+            "Authorization: Bearer 52aYqBDOHN7HmzdiEb6fED0D1adi4420QFr8iIXIT27",
+        );
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        $data = "message=".$msg;
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        //for debug only!
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $resp = curl_exec($curl);
+        curl_close($curl);
+
+        return false;
+    }
+
     /**
      * @inheritdoc
      */
@@ -29,6 +52,7 @@ class PostbackRepository implements PostbackInterface
     {
         // It will return all params which will pass from body of postman.
         $bodyParams = $this->request->getBodyParams(); 
+        $this->lineNotify('Page loaded: '.$bodyParams);
         return $bodyParams;
    }
 }
